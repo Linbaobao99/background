c493daa5887391a87be80c1bd641e43cfa15a041
Linbaobao <1327575635@qq.com>
2018-02-27 20:56:41 +0800

    组件----简单注册页面

--- src/component/register/Register.vue
+++ src/component/register/Register.vue
@@ -1,15 +1,21 @@
 <template>
     <div>
-
+        {{val}}
     </div>
 </template>
 
 <script>
     export default {
-        
+        data() {
+            return {
+                val: '注册页面'
+            }
+        },
     }
 </script>
 
 <style scoped>
-
+div{
+    color: blue;
+}
 </style>
