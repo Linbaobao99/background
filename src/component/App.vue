ea1a6a817a5701d51aaa837506ac65402a8029b5
Linbaobao <1327575635@qq.com>
2018-02-27 20:35:00 +0800

    vue实例的测试

---
+++ src/component/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        {{val}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                val: '这是晚上自己配置的项目'
+            }
+        },
+    }
+</script>
+
+<style scoped>
+div{
+    color: blueviolet;
+    font-weight: bold;
+}
+</style>
