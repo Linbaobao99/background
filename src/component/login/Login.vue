f5f37d706243dee2dee310e18a92f45028a66b76
Linbaobao <1327575635@qq.com>
2018-02-27 20:54:08 +0800

    组件----简单登录页面

---
+++ src/component/login/Login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        {{val}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                val: '登录页面'
+            }
+        },
+    }
+</script>
+
+<style scoped>
+div{
+    color: brown;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 
 import App from './component/App.vue'
 
+import router from './router'
+
+
+
 new Vue({
     el:'#app',
-    render: createElement => createElement(App)
+    render: createElement => createElement(App),
+    router:router
 })
