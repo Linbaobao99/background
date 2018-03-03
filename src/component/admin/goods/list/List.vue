1f6b10470802534e4013feb952f3f1914a14cb30
Linbaobao <1327575635@qq.com>
2018-03-03 15:07:06 +0800

    商品管理>内容管理的分页功能

--- src/component/admin/goods/list/List.vue
+++ src/component/admin/goods/list/List.vue
@@ -9,7 +9,7 @@
     <!-- 按钮和搜索框 -->
     <section class="list_btns">
       <el-button plain size="mini" icon="el-icon-plus">新增</el-button>
-      <el-button plain size="mini" icon="el-icon-check">全选</el-button>
+      <el-button plain size="mini" icon="el-icon-check" @click="all">全选</el-button>
       <el-button plain size="mini" icon="el-icon-close" @click="del">删除</el-button>
       <div class="demo-input-suffix list_right">
         <el-input placeholder="请输入商品名称" prefix-icon="el-icon-search" size="mini" v-model="searchData.searchvalue" @blur="search">
@@ -24,9 +24,16 @@
       <!-- label用来设置当前列的表头 -->
       <!-- 里面的template用来自定义表格中的内容和数据 -->
       <el-table-column label="标题">
+
         <template slot-scope="scope">
-          <router-link style="color:#68f" :to="{name:'goodsDetail'}">{{scope.row.title}}</router-link>
+          <el-tooltip class="item" effect="dark" placement="right">
+            <div slot="content">
+              <img style="width:150px" :src="scope.row.imgurl" alt="商品图片">
+            </div>
+            <router-link style="color:#68f" :to="{path:`detail/${scope.row.id}`}">{{scope.row.title}}</router-link>
+          </el-tooltip>
         </template>
+
       </el-table-column>
       <!-- 当前列要展示对象中的那个字段的值,就配置prop属性为那个字段名 -->
       <el-table-column prop="categoryname" label="所属类别" width="100">
@@ -38,7 +45,11 @@
       <el-table-column prop="sell_price" label="销售价" width="100" show-overflow-tooltip>
       </el-table-column>
       <el-table-column label="属性" width="100" show-overflow-tooltip>
-        <template slot-scope="scope">三个图标</template>
+        <template slot-scope="scope">
+          <i :class="['el-icon-picture',scope.row.is_slide==1?'active':'','icon']"></i>
+          <i :class="['el-icon-upload',scope.row.is_top==1?'active':'','icon']"></i>
+          <i :class="['el-icon-star-on',scope.row.is_hot==1?'active':'','icon']"></i>
+        </template>
       </el-table-column>
       <el-table-column label="操作" width="100">
         <template slot-scope="scope">
@@ -47,10 +58,9 @@
       </el-table-column>
     </el-table>
     <!-- 分页 -->
-    <!-- <div class="block">
-      <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="currentPage4" :page-sizes="[100, 200, 300, 400]" :page-size="100" layout="total, sizes, prev, pager, next, jumper" :total="400">
-      </el-pagination>
-    </div> -->
+    <!-- total 用来设定数据总数, current-page同来设定当前页,page-sizes用来设定当前每页数量-->
+    <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="searchData.pageIndex" :page-sizes="[2, 4, 6, 8]" :page-size="searchData.pageSize" layout="total, sizes, prev, pager, next, jumper" :total="searchData.total">
+    </el-pagination>
   </div>
 </template>
 
@@ -58,12 +68,13 @@
 export default {
   data() {
     return {
-      searchData:{
-        pageIndex:1,
-        pageSize:10,
-        searchvalue:''
+      searchData: {
+        pageIndex: 1,
+        pageSize: 2,
+        searchvalue: "",
+        total:0
       },
-      selectionGoodsList:[],
+      selectionGoodsList: [],
       tableData3: [
         {
           date: "2016-05-03",
@@ -81,32 +92,43 @@ export default {
   },
 
   methods: {
+    //全选
+    all() {
+      document.querySelector(".el-checkbox__original").click();
+    },
     //删除
     handleSelectionChange(val) {
       this.selectionGoodsList = val;
     },
-    del(){
-      let delIds = this.selectionGoodsList.map(v=>v.id)//提取所有被选中的商品的id
-      this.$axios.get(this.$api.gsDel+delIds).then((res)=>{
-        if(res.data.status==0){
+    del() {
+      let delIds = this.selectionGoodsList.map(v => v.id); //提取所有被选中的商品的id
+      this.$axios.get(this.$api.gsDel + delIds).then(res => {
+        if (res.data.status == 0) {
           this.getGoodsData();
         }
-      })
+      });
     },
     //搜索
-    search(){
-      this.getGoodsData()
+    search() {
+      this.getGoodsData();
     },
     getGoodsData() {
       //这个接口需要指定页,pageSize指定每页数量,searchvalue用于商品搜索
-      let api = this.$api.gsList + "?pageIndex="+this.searchData.pageIndex+"&pageSize="+this.searchData.pageSize+"&searchvalue="+this.searchData.searchvalue;
-      this.$axios
-        .get(api)
-        .then(res => {
-          if (res.data.status == 0) {
-            this.tableData3 = res.data.message;
-          }
-        });
+      let api =
+        this.$api.gsList +
+        "?pageIndex=" +
+        this.searchData.pageIndex +
+        "&pageSize=" +
+        this.searchData.pageSize +
+        "&searchvalue=" +
+        this.searchData.searchvalue;
+      this.$axios.get(api).then(res => {
+        if (res.data.status == 0) {
+          this.tableData3 = res.data.message;
+          //把后端接口返回的数据总量赋值给分页组件使用
+          this.searchData.total=res.data.totalcount;
+        }
+      });
     },
     toggleSelection(rows) {
       if (rows) {
@@ -118,12 +140,14 @@ export default {
       }
     },
     /* 分页 */
-    // handleSizeChange(val) {
-    //   console.log(`每页 ${val} 条`);
-    // },
-    // handleCurrentChange(val) {
-    //   console.log(`当前页: ${val}`);
-    // }
+    handleSizeChange(size) {
+      this.searchData.pageSize = size;
+      this.getGoodsData();
+    },
+    handleCurrentChange(page) {
+      this.searchData.pageIndex = page;
+      this.getGoodsData();
+    }
   },
   //页面一上来就自动调用接口获取表格数据进行展示
   created() {
@@ -148,7 +172,14 @@ export default {
   }
   .el-table {
     margin-top: 20px;
-    margin-bottom:20px;
+    margin-bottom: 20px;
+  }
+  .active {
+    color: #000;
+    font-weight: bold;
+  }
+  i.icon {
+    font-size: 20px;
   }
 }
 </style>
