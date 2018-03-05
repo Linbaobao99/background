d29e05ff3d7164f9b0b8e90639ab240231c698fe
Linbaobao <1327575635@qq.com>
2018-03-05 16:06:26 +0800

    验证登录

--- src/router/index.js
+++ src/router/index.js
@@ -12,20 +12,53 @@ import GoodsComment from '../component/admin/goods/comment/Comment.vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css'
 
-const goods =[
-    { name: 'list', path: 'goods/list', component: GoodsList},
-    { name: 'detail', path: 'goods/detail/:id', component: GoodsDetail},
-    { name: 'comment', path: 'goods/comment', component: GoodsComment}
+const goods = [
+    { name: 'list', path: 'goods/list', component: GoodsList },
+    { name: 'detail', path: 'goods/detail/:id', component: GoodsDetail },
+    { name: 'comment', path: 'goods/comment', component: GoodsComment }
 ]
 
 Vue.use(VueRouter)
 Vue.use(ElementUI)
 
 //导出路由实例
-export default new VueRouter({
-    routes:[
-        {name:'login',path:'/login',component:Login},
-        { name: 'admin', path: '/admin', component: Admin, children: [...goods]}
+let router = new VueRouter({
+    routes: [
+        { name: 'login', path: '/login', component: Login },
+        { name: 'admin', path: '/admin', component: Admin, children: [...goods] }
     ]
 })
+//路由守卫
+router.beforeEach((to, from, next) => {
+    Vue.prototype.$axios.get(Vue.prototype.$api.islogin).then((res) => {
+        let isLogin = false;
+        //判断用户时候登录过
+        if (res.data.code == "logined") {
+            isLogin = true;
+        }
 
+        //访问登录页面
+        if (to.name == "login") {
+            if (isLogin) {
+                //如果登录过就直接跳转后台管理页面
+                next({ name: 'login' });
+            } else {
+                // alert("你还没登录,请先登录.","提示");
+                //没有登录过就允许继续访问
+                next();
+            }
+        }
+
+        //访问后台管理页面
+        if (to.name != "login") {
+            if (isLogin) {
+                // alert("你已登录过,无需再登录.")
+                //如果登录过就允许访问
+                next();
+            } else {
+                next({ name: 'login', query: { next: to.fullPath } })
+            }
+        }
+    })
+})
+export default router;
